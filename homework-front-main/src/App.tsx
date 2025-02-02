6035f49ee02cabbee1377aacf4e47c187e35476b
Yuliya Kotova <yulya9449@yandex.ru>
2025-02-03 00:18:16 +0300

    feat: complete home work 2

--- homework-front-main/src/App.tsx
+++ homework-front-main/src/App.tsx
@@ -1,11 +1,12 @@
 import './App.css';
 import { HW1 } from './HW/1/HW1';
+import { HW2 } from './HW/2/HW2';
 
 export const App = () => {
   return (
     <>
       <HW1 />
-      {/* <HW2 /> */}
+      <HW2 />
       {/* <HW3 /> */}
       {/* <HW4 /> */}
     </>
