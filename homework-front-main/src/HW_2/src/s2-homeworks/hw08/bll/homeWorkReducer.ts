b7b0eebd489e4857fad7e6c5a4f2be23512d7f48
Yuliya9449 <yulya9449@yandex.ru>
2025-06-27 23:44:45 +0300

    complete homework 8 WednesDay

--- homework-front-main/src/HW_2/src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ homework-front-main/src/HW_2/src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -1,20 +1,30 @@
-import {UserType} from '../HW8'
+import type { UserType } from '../HW8';
 
 type ActionType =
-    | { type: 'sort'; payload: 'up' | 'down' }
-    | { type: 'check'; payload: number }
+  | { type: 'sort'; payload: 'up' | 'down' }
+  | { type: 'check'; payload: number };
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
-    switch (action.type) {
-        case 'sort': { // by name
+export const homeWorkReducer = (
+  state: UserType[],
+  action: ActionType,
+): UserType[] => {
+  switch (action.type) {
+    case 'sort': {
+      // by name
+      if (action.payload === 'up') {
+        return [...state.sort((a, b) => (a.name > b.name ? 1 : -1))];
+      }
 
-            return state // need to fix
-        }
-        case 'check': {
+      if (action.payload === 'down') {
+        return [...state.sort((a, b) => (b.name > a.name ? 1 : -1))];
+      }
 
-            return state // need to fix
-        }
-        default:
-            return state
+      return state;
     }
-}
+    case 'check': {
+      return state.filter((s) => s.age >= 18);
+    }
+    default:
+      return state;
+  }
+};
